9d0d48063c6e3ce2460f4529addec72fade0f3df
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-13 23:57:12 -0700

    refactoring sever.js and middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,14 @@
-module.exports = {
+// Use module exports to expose middleware
+
+var middleware = {
     requireAuthentication: function (req, res, next) {
-        console.log('private route hit !')
+        console.log('private route hit!');
         next();
     },
     logger: function (req, res, next) {
-        console.log('Request ' + ' ' + new Date().toString() + req.method + ' ' + req.originalUrl);
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
     }
-}
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var middleware = require('./middleware')
+
+var middleware = require('./middleware.js');
+
 app.use(middleware.logger);
+
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-    res.send('About us !');
+    res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function () {
-    console.log('express server started at ' + PORT);
+    console.log('Express server started on port ' + PORT + '!');
 });
