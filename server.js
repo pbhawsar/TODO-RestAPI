cf6897a587cc74e4b4cd25d8d46d94c5639f79fb
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-13 23:12:37 -0700

    updated middleware

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express =  require('express');
 var app = express();
 var PORT = 3000;
-var middleware = {
-    requireAuthentication: function(req,res,next){
-       console.log('private route hit !')
-        next();
-    },
-    logger: function(req,res,next){
-        console.log('Request ' +' '+ new Date().toString()+ req.method+' '+req.originalUrl);
-    }
-}
+var middleware = require('./middleware')
 app.use(middleware.logger);
 app.get('/about',middleware.requireAuthentication,function(req,res){
     res.send('About us !');
