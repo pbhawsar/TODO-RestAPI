023ae9a9a09006e06b2bcf83db266e6f43273728
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-15 16:21:53 -0700

    making todo app

--- middleware.js
+++ middleware.js
@@ -1,10 +1,7 @@
 // Use module exports to expose middleware
 
 var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
+
     logger: function (req, res, next) {
         console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
         next();
--- server.js
+++ server.js
@@ -1,17 +1,55 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
+    var express = require('express');
+    var app = express();
+    var PORT = process.env.PORT || 3000;
+    var todos = [{
+        id: 1,
+        description: 'Do NodeJS programming',
+        completed: false
+    }, {
+        id: 2,
+        description: 'Go to market',
+        complete: false
+    }, {
+        id: 3,
+        description: 'Eat food.....',
+        complete: true
+    }];
 
-var middleware = require('./middleware.js');
+    var middleware = require('./middleware.js');
 
-app.use(middleware.logger);
+    app.use(middleware.logger);
 
-app.get('/about', middleware.requireAuthentication, function (req, res) {
-    res.send('About Us!');
-});
+    //app.get('/', function (req, res) {
+    //    res.send('TODO API Root !');
+    //});
 
-app.use(express.static(__dirname + '/public'));
+    // GET /todos
+    app.get('/todos', function (req, res) {
+        res.json(todos);
+    });
+    // GET /todos/:id
+    app.get('/todos/:id', function (req, res) {
+        var todoId = parseInt(req.params.id, 10);
+        var matchedTodo;
+        // iterate over todos array and find match
+        todos.map(function (obj) {
+            if (todoId === obj.id) {
+                matchedTodo = obj;
+            }
 
-app.listen(PORT, function () {
-    console.log('Express server started on port ' + PORT + '!');
-});
+        })
+        // object is truthy
+        // undefined is falsy
+        if (matchedTodo) {
+            res.json(matchedTodo);
+        } else {
+            res.status(404).send();
+        }
+
+    });
+
+    app.use(express.static(__dirname + '/public'));
+
+    app.listen(PORT, function () {
+        console.log('Express server started on port ' + PORT + '!');
+    });
