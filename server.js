a08d6a99a1292ec92cc086e155a25f31be933077
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-15 20:02:42 -0700

    sanitize data

--- server.js
+++ server.js
@@ -1,63 +1,62 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var middleware = require('./middleware.js');
+    var express = require('express');
+    var bodyParser = require('body-parser');
+    var middleware = require('./middleware.js');
+    var _ = require('underscore');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
+    var app = express();
+    var PORT = process.env.PORT || 3000;
+    var todos = [];
+    var todoNextId = 1;
 
-app.use(bodyParser.json());
-app.use(middleware.logger);
+    app.use(bodyParser.json());
+    app.use(middleware.logger);
 
-// specify the path for static file
-app.use(express.static(__dirname + '/public'));
+    // specify the path for static file
+    app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html');
-});
+    app.get('/', function (req, res) {
+        res.sendFile('index.html');
+    });
 
-// GET /todos
-app.get('/todos', function (req, res) {
-    res.json(todos);
-});
+    //GET /todos
+    app.get('/todos', function (req, res) {
+        res.json(todos);
+    });
 
-// GET /todos/:id
-app.get('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    //GET /todos/:id
+    app.get('/todos/:id', function (req, res) {
+        var todoId = parseInt(req.params.id, 10);
+        var matchedTodo = _.findWhere(todos,{id:todoId});
 
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
+        if (matchedTodo) {
+            res.json(matchedTodo);
+        } else {
+            res.status(404).send();
         }
     });
 
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
-});
-
-// POST /todos
-app.post('/todos', function (req, res) {
-    var body = req.body;
-
-    // add id field
-    body.id = todoNextId++;
-    body.description="Got it !"
-
-    // push body into array
-    todos.push(body);
-
-    res.json(body);
-});
+    //POST /todos
+    app.post('/todos', function (req, res) {
+        // sanitize the date to include description and completed only
+        var body =   _.pick(req.body,'description','completed');
 
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
-});
+        if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+            return res.status(400).send();
+        }
+
+        // add id field
+        body.id = todoNextId++;
+        body.description =body.description.trim();
+        // push body into array
+        todos.push(body);
+        res.json(body);
+    });
+
+
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 
 
 
