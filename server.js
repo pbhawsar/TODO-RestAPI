1ba1af8eae240c1d5f2cf3ac0b4b8fcc71739713
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-15 21:05:06 -0700

    adding delete rest api

--- server.js
+++ server.js
@@ -53,6 +53,18 @@
         res.json(body);
     });
 
+    //DELETE /todos/:id
+    app.delete('/todos/:id',function(req, res){
+        var todoId = parseInt(req.params.id, 10);
+        var matchedTodo = _.findWhere(todos,{id:todoId});
+        if(!matchedTodo){
+            res.status(404).json({"error":"no matching todo id found to be deleted !"});
+        }else{
+            todos= _.without(todos,matchedTodo);
+            res.json(matchedTodo);
+        }
+
+    });
 
     app.listen(PORT, function () {
         console.log('Express listening on port ' + PORT + '!');
