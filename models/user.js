148ab8661055448fb125982c0fb6b701c5139f28
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-18 16:10:46 -0700

    adding hooks for email lowercase

--- models/user.js
+++ models/user.js
@@ -1,19 +1,28 @@
-module.exports = function(sequelize,DataTypes){
-  return sequelize.define('user',{
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-   password: {
-     type: DataTypes.STRING,
-     allowNull: false,
-     validate: {
-       len: [7,100]
-     }
-   }
- }
-)}
+module.exports = function (sequelize, DateTypes) {
+	return sequelize.define('user', {
+		email: {
+			type: DateTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DateTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
+	});
+};
