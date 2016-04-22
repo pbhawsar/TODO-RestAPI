1bab4becead80625fb8bf7c4a396a33dda23a952
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-22 00:14:42 -0700

    final commit

--- server.js
+++ server.js
@@ -10,20 +10,18 @@ var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
+
 app.use(bodyParser.json());
 
-// specify the path for static file
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-	res.sendFile('index.html');
+app.get('/', function(req, res) {
+	res.send('Todo API Root');
 });
 
 // GET /todos?completed=false&q=work
-app.get('/todos',middleware.requireAuthentication, function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {
-		userId:req.user.get('id')
+		userId: req.user.get('id')
 	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
@@ -48,16 +46,15 @@ app.get('/todos',middleware.requireAuthentication, function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findOne({
 		where: {
-			id:todoId,
+			id: todoId,
 			userId: req.user.get('id')
 		}
-	})
-	.then(function(todo) {
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -69,26 +66,22 @@ app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
 });
 
 // POST /todos
-app.post('/todos',middleware.requireAuthentication, function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body)
-	.then(function(todo) {
-		req.user.addTodo(todo)
-		.then(function () {
+	db.todo.create(body).then(function(todo) {
+		req.user.addTodo(todo).then(function () {
 			return todo.reload();
-		})
-		.then(function (todo){
+		}).then(function (todo) {
 			res.json(todo.toJSON());
 		});
-
 	}, function(e) {
 		res.status(400).json(e);
 	});
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id',middleware.requireAuthentication, function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -110,7 +103,7 @@ app.delete('/todos/:id',middleware.requireAuthentication, function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
@@ -128,8 +121,7 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
 			id: todoId,
 			userId: req.user.get('id')
 		}
-	})
-	.then(function(todo) {
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -157,20 +149,32 @@ app.post('/users', function (req, res) {
 // POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
-	db.user.authenticate(body).then(function (user){
+	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
-		if(token){
-			res.header('Auth',token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-	},function (){
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
 	});
 });
 
-db.sequelize.sync({force:false}).then(function() {
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
+	});
+});
+
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
