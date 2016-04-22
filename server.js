923e08ea82b0fd54bbccdbb71abaf4a2c76df568
Pranay Bhawsar <pbhawsar@paypal.com>
2016-04-21 22:33:19 -0700

    updating the request with user specific authentication

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+	res.sendFile('index.html');
 });
 
 // GET /todos?completed=false&q=work
 app.get('/todos',middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {
-		userId:req.user.get('id');
+		userId:req.user.get('id')
 	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
@@ -51,7 +51,13 @@ app.get('/todos',middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id:todoId,
+			userId: req.user.get('id')
+		}
+	})
+	.then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -68,13 +74,13 @@ app.post('/todos',middleware.requireAuthentication, function(req, res) {
 
 	db.todo.create(body)
 	.then(function(todo) {
-		 req.user.addTodo(todo)
-		 .then(function () {
-			 return todo.reload();
-		 })
-		 .then(function (todo){
-			 	res.json(todo.toJSON());
-		 });
+		req.user.addTodo(todo)
+		.then(function () {
+			return todo.reload();
+		})
+		.then(function (todo){
+			res.json(todo.toJSON());
+		});
 
 	}, function(e) {
 		res.status(400).json(e);
@@ -87,7 +93,8 @@ app.delete('/todos/:id',middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -116,7 +123,13 @@ app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	})
+	.then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
